12b460a2f40000f4f8d439f8586e877d2976e2ad
Danilo Pinotti <danilopinotti@hotmail.com>
2022-01-18 19:00:54 -0300

    Configurar kong e rabbitmq

--- backend/client-ms/routes/web.php
+++ backend/client-ms/routes/web.php
@@ -14,6 +14,17 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
+    \App\Jobs\UserTrackedObject::dispatch(123);
+
+    return response()->json([
+        'status' => 'ok',
+        'service' => 'client-ms',
+    ]);
+});
+
+Route::get('object-track/{object_id}', function ($objectId) {
+    \App\Jobs\UserTrackedObject::dispatch($objectId);
+
     return response()->json([
         'status' => 'ok',
         'service' => 'client-ms',
